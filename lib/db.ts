4f91728a7cf333b13e60594c1f61419119c1f630
Lalbabu-Coder <rishisingh765453@gmail.com>
2026-04-19 01:23:34 +0530

    fix mongodb cache connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,18 @@
+
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-let cached = (global as any).mongoose || { conn: null };
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export default async function connectDB() {
   if (cached.conn) return cached.conn;
 
-  cached.conn = await mongoose.connect(MONGODB_URI);
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
   return cached.conn;
 }
+
